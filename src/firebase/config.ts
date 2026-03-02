dda343b8368611da20cb99d81a2fefe5dba52230
Abdul-Qawi Laniyan <oladotunlaniyan@gmail.com>
2026-03-02 13:49:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2995108879-ba0a1",
+  "appId": "1:883862256343:web:649a7d455404976ac64e92",
+  "apiKey": "AIzaSyA99NQSjuzWmPqqxlSLitveWsnRBBMrby0",
+  "authDomain": "studio-2995108879-ba0a1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "883862256343"
+};
