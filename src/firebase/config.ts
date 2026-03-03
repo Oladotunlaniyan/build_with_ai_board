09b70c92c2f4c54335e3685654df7c732c05ae4c
Cypher <oladotunlaniyan@gmail.com>
2026-03-03 06:52:27 +0100

    fix: publicly accessible api keys

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2995108879-ba0a1",
-  "appId": "1:883862256343:web:649a7d455404976ac64e92",
-  "apiKey": "AIzaSyA99NQSjuzWmPqqxlSLitveWsnRBBMrby0",
-  "authDomain": "studio-2995108879-ba0a1.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "883862256343"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
